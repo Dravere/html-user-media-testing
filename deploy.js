f3fc00032c625340ac4627954bcd2ee989dd7a0b
Florian Graf <florian.graf@fmgsoftware.ch>
2021-04-13 11:17:39 +0200

    new github pages deploy script

---
+++ deploy.js
@@ -0,0 +1,29 @@
+const { exec } = require('child_process');
+
+async function execute(cmd) {
+  return new Promise((resolve, reject) => {
+    console.log(cmd);
+    exec(cmd, (err, stdout, stderr) => {
+      console.log(stdout);
+      if(err) {
+        console.log(stderr);
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+async function run() {
+  await execute('ng build --base-href /html-user-media-testing/ --prod');
+  await execute('git add dist');
+  await execute('git commit -m "GitHub Pages Deploy Version ' + new Date().toISOString() + '"');
+  await execute('git subtree push --prefix dist/HtmlUserMediaTesting origin gh-pages');
+}
+
+run().then(() => {
+  console.log("Deployed successfully!");
+}).catch((err) => {
+  console.log("Deployment failed: ", err);
+});
