13503dd53a6f819cf67f532812f1d21dfff906cc
Florian Graf <florian.graf@fmgsoftware.ch>
2021-04-14 17:45:27 +0200

    creating test links on opus recorder works now

--- src/app/opus-recorder-test/opus-recorder-test.component.ts
+++ src/app/opus-recorder-test/opus-recorder-test.component.ts
@@ -2,15 +2,34 @@ import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {RecordAudioTestComponent} from '../record-audio-test/record-audio-test.component';
 import Recorder from 'opus-recorder';
 import encoderPath from 'opus-recorder/dist/waveWorker.min.js';
+import {SettingsUrl} from '../SettingsUrl';
 
 @Component({
   selector: 'app-opus-recorder-test',
   templateUrl: './opus-recorder-test.component.html',
   styleUrls: ['./opus-recorder-test.component.scss']
 })
-export class OpusRecorderTestComponent implements OnDestroy {
+export class OpusRecorderTestComponent implements OnInit, OnDestroy {
   private recorder;
   @ViewChild('audioTestBase', {static: true}) audioTestBase: RecordAudioTestComponent;
+
+  ngOnInit(): void {
+    const settingsUrl = new SettingsUrl();
+    const testName = settingsUrl.getParam(['tn', 'testName']);
+    if (testName) {
+      this.audioTestBase.testName = testName;
+    }
+
+    const userMediaConstraints = settingsUrl.getJsonParam(['umc', 'userMediaConstraints']);
+    if (userMediaConstraints) {
+      this.audioTestBase.userMediaConstraints = userMediaConstraints;
+    } else {
+      this.audioTestBase.userMediaConstraints = {
+        video: false,
+        audio: true
+      };
+    }
+  }
 
   ngOnDestroy(): void {
     if (this.recorder) {
@@ -18,15 +37,31 @@ export class OpusRecorderTestComponent implements OnDestroy {
     }
   }
 
-  public copyTestLink(): void {
+  public async copyTestLink(): Promise<void> {
+    await navigator.clipboard.writeText(this.createTestLink());
   }
 
   public showQrCode(): void {
+    const qrCodeUrl = new URL('https://zxing.org/w/chart?cht=qr&chs=350x350&chld=L&choe=UTF-8&chl=');
+    qrCodeUrl.searchParams.set('chl', this.createTestLink());
+    this.audioTestBase.qrCodeUrl = qrCodeUrl.href;
+  }
+
+  private createTestLink(): string {
+    const settingsUrl = new SettingsUrl(new URL('', window.location.protocol + '//' + window.location.host + window.location.pathname));
+    settingsUrl.setParam('t', 'o');
+    settingsUrl.setParam('tn', this.audioTestBase.testName);
+    settingsUrl.setJsonParam('umc', this.audioTestBase.userMediaConstraints);
+    return settingsUrl.getHref();
   }
 
   public async startRecording(): Promise<void> {
     this.audioTestBase.logText = '';
-    this.recorder = new Recorder({encoderPath});
+    const recorderConfig = Object.assign({
+      mediaTrackConstraints: this.audioTestBase.userMediaConstraints.audio,
+      encoderPath
+    }, {});
+    this.recorder = new Recorder(recorderConfig);
     this.recorder.ondataavailable = (typedArray) => {
       this.audioTestBase.appendLogLine('Audio data prepared');
       this.audioTestBase.audioUrl = URL.createObjectURL(new Blob([typedArray], {type: 'audio/wav'}));
